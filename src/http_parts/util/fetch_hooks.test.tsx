bb7574a2c41515664553882a6b00ace450815149
河原 <106893036+taka-kawah@users.noreply.github.com>
2025-06-08 08:42:14 +0000

    useHttpTrigger追加

--- src/http_parts/util/fetch_hooks.test.tsx
+++ src/http_parts/util/fetch_hooks.test.tsx
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, test } from "vitest";
 import { z } from "zod";
-import { renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 import { fetchMock } from "../../vitest.setup";
 
 describe("useHttpGet", () => {
@@ -180,5 +180,236 @@ describe("useHttpPost", () => {
     expect(result.current.data).toBeNull();
     expect(result.current.error).not.toBeNull();
     expect(result.current.error!.message).toContain("HTTP error! status: 500");
+  });
+});
+
+describe("useHttpGetTrigger", () => {
+  beforeEach(() => fetchMock.mockReset());
+
+  test("正常系: fetch成功、データ型一致、Authorizagionヘッダあり", async () => {
+    const schema = z.object({ id: z.number(), name: z.string() }).strict();
+    type User = z.infer<typeof schema>;
+    const data: User = { id: 1, name: "taro" };
+
+    const header = new Headers({ Authorization: "Bearer test-token" });
+    fetchMock.mockResolvedValueOnce({
+      ok: true,
+      json: async () => data,
+      headers: header,
+    });
+    const { useHttpGetTrigger } = await import("./fetch_hooks");
+    const { result } = renderHook(() => useHttpGetTrigger<User>());
+
+    act(() => {
+      result.current.getTrigger("example.com", "/path", schema);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(true);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.data).toEqual(data);
+      expect(result.current.error).toBeNull();
+      expect(result.current.resHeader).not.toBeNull();
+      expect(result.current.resHeader?.get("Authorization")).toBe(
+        "Bearer test-token"
+      );
+    });
+  });
+
+  test("正常系: fetch成功、データ型一致", async () => {
+    const schema = z.object({ id: z.number(), name: z.string() }).strict();
+    type User = z.infer<typeof schema>;
+    const data: User = { id: 1, name: "taro" };
+
+    fetchMock.mockResolvedValueOnce({ ok: true, json: async () => data });
+    const { useHttpGetTrigger } = await import("./fetch_hooks");
+    const { result } = renderHook(() => useHttpGetTrigger<User>());
+
+    act(() => {
+      result.current.getTrigger("example.com", "/path", schema);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(true);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.data).toEqual(data);
+      expect(result.current.error).toBeNull();
+    });
+  });
+
+  test("異常系: fetch成功、データ型不一致", async () => {
+    const schema = z.object({ id: z.number(), name: z.string() }).strict();
+    type User = z.infer<typeof schema>;
+    const data = { id: 1, name: "taro", age: 5 };
+
+    fetchMock.mockResolvedValueOnce({ ok: true, json: async () => data });
+    const { useHttpGetTrigger } = await import("./fetch_hooks");
+    const { result } = renderHook(() => useHttpGetTrigger<User>());
+
+    act(() => {
+      result.current.getTrigger("example.com", "/path", schema);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(true);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.data).toBeNull;
+      expect(result.current.error).not.toBeNull();
+      expect(result.current.error!.name).toBe("ZodError");
+    });
+  });
+
+  test("異常系: fetch失敗", async () => {
+    const schema = z.object({}).strict();
+    fetchMock.mockResolvedValueOnce({
+      ok: false,
+      status: 500,
+      json: async () => ({ message: "internal server error" }),
+    });
+
+    const { useHttpGetTrigger } = await import("./fetch_hooks");
+    const { result } = renderHook(() => useHttpGetTrigger());
+
+    act(() => {
+      result.current.getTrigger("example.com", "/path", schema);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(true);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.data).toBeNull;
+      expect(result.current.error).not.toBeNull();
+      expect(result.current.error!.message).toContain(
+        "HTTP error! status: 500"
+      );
+    });
+  });
+});
+
+describe("useHttpPostTrigger", () => {
+  beforeEach(() => fetchMock.mockReset());
+  const payload = {};
+
+  test("正常系: fetch成功、データ型一致、Authorizationヘッダあり", async () => {
+    const schema = z.object({ id: z.number(), name: z.string() }).strict();
+    type User = z.infer<typeof schema>;
+    const data: User = { id: 1, name: "taro" };
+
+    const header = new Headers({ Authorization: "Bearer test-token" });
+    fetchMock.mockResolvedValueOnce({
+      ok: true,
+      json: async () => data,
+      headers: header,
+    });
+    const { useHttpPostTrigger } = await import("./fetch_hooks");
+    const { result } = renderHook(() => useHttpPostTrigger<User>());
+
+    act(() => {
+      result.current.postTrigger("example.com", "/path", payload, schema);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(true);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.data).toEqual(data);
+      expect(result.current.error).toBeNull();
+      expect(result.current.resHeader).not.toBeNull();
+      expect(result.current.resHeader?.get("Authorization")).toBe(
+        "Bearer test-token"
+      );
+    });
+  });
+
+  test("正常系: fetch成功、データ型一致", async () => {
+    const schema = z.object({ id: z.number(), name: z.string() }).strict();
+    type User = z.infer<typeof schema>;
+    const data: User = { id: 1, name: "taro" };
+
+    fetchMock.mockResolvedValueOnce({ ok: true, json: async () => data });
+    const { useHttpPostTrigger } = await import("./fetch_hooks");
+    const { result } = renderHook(() => useHttpPostTrigger<User>());
+
+    act(() => {
+      result.current.postTrigger("example.com", "/path", payload, schema);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(true);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.data).toEqual(data);
+      expect(result.current.error).toBeNull();
+    });
+  });
+
+  test("異常系: fetch成功、データ型不一致", async () => {
+    const schema = z.object({ id: z.number(), name: z.string() }).strict();
+    type User = z.infer<typeof schema>;
+    const data = { id: 1, name: "taro", age: 5 };
+
+    fetchMock.mockResolvedValueOnce({ ok: true, json: async () => data });
+    const { useHttpPostTrigger } = await import("./fetch_hooks");
+    const { result } = renderHook(() => useHttpPostTrigger<User>());
+
+    act(() => {
+      result.current.postTrigger("example.com", "/path", payload, schema);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(true);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.data).toBeNull;
+      expect(result.current.error).not.toBeNull();
+      expect(result.current.error!.name).toBe("ZodError");
+    });
+  });
+
+  test("異常系: fetch失敗", async () => {
+    const schema = z.object({}).strict();
+    fetchMock.mockResolvedValueOnce({
+      ok: false,
+      status: 500,
+      json: async () => ({ message: "internal server error" }),
+    });
+
+    const { useHttpPostTrigger } = await import("./fetch_hooks");
+    const { result } = renderHook(() => useHttpPostTrigger());
+
+    act(() => {
+      result.current.postTrigger("example.com", "/path", payload, schema);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(true);
+    });
+
+    await waitFor(() => {
+      expect(result.current.loading).toBe(false);
+      expect(result.current.data).toBeNull;
+      expect(result.current.error).not.toBeNull();
+      expect(result.current.error!.message).toContain(
+        "HTTP error! status: 500"
+      );
+    });
   });
 });
--- src/http_parts/util/fetch_hooks.ts
+++ src/http_parts/util/fetch_hooks.ts
@@ -1,31 +1,10 @@
 import { useEffect, useState } from "react";
 import { ZodSchema } from "zod";
 
-async function httpGet(
-  domain: string,
-  path: string,
-  authToken?: string
-): Promise<Response> {
-  console.log(`GET from: https://${domain}${path}`);
-  const response = await fetch(`https://${domain}${path}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: authToken ? `Bearer ${authToken}` : "",
-    },
-  });
-  if (!response.ok) {
-    console.error("HTTP error response:", response);
-    throw new Error(`HTTP error! status: ${response.status} ${response.body}`);
-  }
-  console.log("HTTP OK response:", response);
-  return response;
-}
-
 export function useHttpGet<T>(
   domain: string,
   path: string,
-  schema: ZodSchema,
+  schema: ZodSchema<T>,
   authToken?: string
 ) {
   const [data, setData] = useState<T | null>(null);
@@ -58,29 +37,6 @@ export function useHttpGet<T>(
   }, [domain, path]);
 
   return { data, error, loading, resHeader };
-}
-
-async function httpPost(
-  domain: string,
-  path: string,
-  payload: any,
-  authToken?: string
-) {
-  console.log(`POST to: https://${domain}${path}`);
-  const response = await fetch(`https://${domain}${path}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: authToken ? `Bearer ${authToken}` : "",
-    },
-    body: JSON.stringify(payload),
-  });
-  if (!response.ok) {
-    console.error("HTTP error response:", response);
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  console.log("HTTP OK response:", response);
-  return response;
 }
 
 export function useHttpPost<T>(
@@ -120,4 +76,115 @@ export function useHttpPost<T>(
   }, [domain, path, payload]);
 
   return { data, error, loading, resHeader };
+}
+
+export function useHttpGetTrigger<T>() {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [resHeader, setResHeader] = useState<Headers | null>(null);
+
+  const getTrigger = async (
+    domain: string,
+    path: string,
+    schema: ZodSchema<T>,
+    authToken?: string
+  ) => {
+    try {
+      setLoading(true);
+      const response = await httpGet(domain, path, authToken);
+      setResHeader(response.headers);
+      const json = await response.json();
+      const result = schema.safeParse(json);
+      if (!result.success) {
+        throw result.error;
+      }
+      setData(result.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError(error as Error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { getTrigger, data, error, loading, resHeader };
+}
+
+export function useHttpPostTrigger<T>() {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [resHeader, setResHeader] = useState<Headers | null>(null);
+
+  const postTrigger = async (
+    domain: string,
+    path: string,
+    payload: any,
+    schema: ZodSchema<T>,
+    authToken?: string
+  ) => {
+    try {
+      setLoading(true);
+      const response = await httpPost(domain, path, payload, authToken);
+      setResHeader(response.headers);
+      const json = await response.json();
+      const result = schema.safeParse(json);
+      if (!result.success) {
+        throw result.error;
+      }
+      setData(result.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError(error as Error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { postTrigger, data, error, loading, resHeader };
+}
+
+async function httpGet(
+  domain: string,
+  path: string,
+  authToken?: string
+): Promise<Response> {
+  console.log(`GET from: https://${domain}${path}`);
+  const response = await fetch(`https://${domain}${path}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: authToken ? `Bearer ${authToken}` : "",
+    },
+  });
+  if (!response.ok) {
+    console.error("HTTP error response:", response);
+    throw new Error(`HTTP error! status: ${response.status} ${response.body}`);
+  }
+  console.log("HTTP OK response:", response);
+  return response;
+}
+
+async function httpPost(
+  domain: string,
+  path: string,
+  payload: any,
+  authToken?: string
+) {
+  console.log(`POST to: https://${domain}${path}`);
+  const response = await fetch(`https://${domain}${path}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: authToken ? `Bearer ${authToken}` : "",
+    },
+    body: JSON.stringify(payload),
+  });
+  if (!response.ok) {
+    console.error("HTTP error response:", response);
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  console.log("HTTP OK response:", response);
+  return response;
 }
